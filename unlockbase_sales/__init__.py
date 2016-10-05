97ee1083afafad5e6537cba4620cc16cb4ef59e7
ilyasProgrammer <rakhimkulov@it-projects.info>
2016-10-05 17:31:04 +0500

    dev improve

--- unlockbase_sales/__init__.py
+++ unlockbase_sales/__init__.py
@@ -10,17 +10,21 @@ _logger.setLevel(logging.DEBUG)
 
 def _set_pos_category(cr, registry):
     root = registry['pos.category'].search(cr, SUPERUSER_ID, [('name', '=', 'Mobile unlock')])
-    mobiles_tools_ids = registry['product.product'].search(cr, SUPERUSER_ID, [('unlock_service', '=', True)])
-    mobiles_tools = registry['product.product'].browse(cr, SUPERUSER_ID, mobiles_tools_ids)
-    for mobile_tool in mobiles_tools:
-        if len(mobile_tool.pos_categ_id) == 0:
-            old_pos_cat = registry['pos.category'].search(cr, SUPERUSER_ID, [('name', '=', mobile_tool.categ_id.name)])
+    mobiles_tools_cats = registry['product.category'].search(cr, SUPERUSER_ID, [('brand_id', '!=', '')])
+    for cat_id in mobiles_tools_cats:
+        mobiles_tools_ids = registry['product.product'].search(cr, SUPERUSER_ID, [('unlock_service', '=', True),
+                                                                                  ('categ_id', '=', cat_id),
+                                                                                  ('pos_categ_id', '=', False)])
+        mobiles_tools = registry['product.product'].browse(cr, SUPERUSER_ID, mobiles_tools_ids)
+        cat = registry['product.category'].browse(cr, SUPERUSER_ID, cat_id)[0]
+        old_pos_cat = registry['pos.category'].search(cr, SUPERUSER_ID, [('name', '=', cat.name)])
+        for mobile_tool in mobiles_tools:
             if len(old_pos_cat) == 0:
                 vals = {'name': mobile_tool.categ_id.name, 'parent_id': root[0]}
                 new_pos_cat = registry['pos.category'].create(cr, SUPERUSER_ID, vals)
                 _logger.info('New pos category created %s' % mobile_tool.categ_id.name)
                 mobile_tool.pos_categ_id = new_pos_cat
             elif len(old_pos_cat) == 1:
-                mobile_tool.pos_categ_id = old_pos_cat
+                mobile_tool.pos_categ_id = old_pos_cat[0]
                 _logger.info('Mobile tool updated %s' % mobile_tool.name)
             cr.commit()
--- unlockbase_sales/unlockbase_sales.py
+++ unlockbase_sales/unlockbase_sales.py
@@ -11,17 +11,17 @@ _logger = logging.getLogger("# " + __name__)
 _logger.setLevel(logging.DEBUG)
 
 
-class PosOrder(models.Model):
-    _inherit = 'pos.order'
-
-    @api.model
-    def create_from_ui(self, cr, uid, context):
-        res = super(PosOrder, self).create_from_ui(cr, uid, context)
-        unlock_product = self.pool['product.product'].search(cr, uid, [('name', '=', 'unlock')])
-        for order in context:
-            for line in order['data']['lines']:
-                pass # TODO
-        return res
+# class PosOrder(models.Model):
+#     _inherit = 'pos.order'
+#
+#     @api.model
+#     def create_from_ui(self):
+#         res = super(PosOrder, self).create_from_ui()
+#         unlock_product = self.pool['product.product'].search([('name', '=', 'unlock')])
+#         for order in self._context:
+#             for line in order['data']['lines']:
+#                 pass # TODO
+#         return res
 
 
 class UnlockBase(models.Model):
@@ -32,18 +32,22 @@ class UnlockBase(models.Model):
     def create_mobiles_tools(self):
         res = super(UnlockBase, self).create_mobiles_tools()
         root = self.env['pos.category'].search([('name', '=', 'Mobile unlock')])
-        mobiles_tools = self.env['product.product'].search([('unlock_service', '=', True)])
-        for mobile_tool in mobiles_tools:
-            old_pos_cat = self.env['pos.category'].search([('name', '=', mobile_tool.categ_id.name)])
-            if len(old_pos_cat) == 0:
-                vals = {'name': mobile_tool.categ_id.name, 'parent_id': root.id}
-                new_pos_cat = self.env['pos.category'].create(vals)
-                _logger.info('New pos category created %s' % mobile_tool.categ_id.name)
-                mobile_tool.pos_categ_id = new_pos_cat
-            elif len(old_pos_cat) == 1:
-                mobile_tool.pos_categ_id = old_pos_cat
-                _logger.info('Mobile tool updated %s' % old_pos_cat.name)
-
+        mobiles_tools_cats = self.env['product.category'].search([('brand_id', '!=', '')])
+        for cat in mobiles_tools_cats:
+            mobiles_tools = self.env['product.product'].search([('unlock_service', '=', True),
+                                                                    ('categ_id', '=', cat.id),
+                                                                    ('pos_categ_id', '=', False)])
+            old_pos_cat = self.env['pos.category'].search([('name', '=', cat.name)])
+            for mobile_tool in mobiles_tools:
+                if len(old_pos_cat) == 0:
+                    vals = {'name': mobile_tool.categ_id.name, 'parent_id': root.id}
+                    new_pos_cat = self.env['pos.category'].create(vals)
+                    _logger.info('New pos category created %s' % mobile_tool.categ_id.name)
+                    mobile_tool.pos_categ_id = new_pos_cat.id
+                elif len(old_pos_cat) == 1:
+                    mobile_tool.pos_categ_id = old_pos_cat.id
+                    _logger.info('Mobile tool updated %s' % mobile_tool.name)
+                self.env.cr.commit()
         return res
 
 
@@ -51,22 +55,22 @@ class PosOrder(models.Model):
     _inherit = 'pos.order'
 
     unlockbase_order_state = fields.Selection([('draft', 'Draft'), ('placed', 'Placed'), ('validated', 'Validated')])
-    unlockbase_network = fields.Char()
-    unlockbase_mobile = fields.Char()
-    unlockbase_provider = fields.Char()
-    unlockbase_pin = fields.Char()
-    unlockbase_kbh = fields.Char()
-    unlockbase_mep = fields.Char()
-    unlockbase_prd = fields.Char()
-    unlockbase_sn = fields.Char()
-    unlockbase_secro = fields.Char()
-    unlockbase_reference = fields.Char()
-    unlockbase_servicetag = fields.Char()
-    unlockbase_icloudemail = fields.Char()
-    unlockbase_icloudphone = fields.Char()
-    unlockbase_icloududid = fields.Char()
-    unlockbase_type = fields.Char()
-    unlockbase_locks = fields.Char()
+    unlockbase_network = fields.Char(string='network', default='AT&T')
+    unlockbase_mobile = fields.Char(string='mobile')
+    unlockbase_provider = fields.Char(string='provider')
+    unlockbase_pin = fields.Char(string='pin')
+    unlockbase_kbh = fields.Char(string='kbh')
+    unlockbase_mep = fields.Char(string='mep')
+    unlockbase_prd = fields.Char(string='prd')
+    unlockbase_sn = fields.Char(string='sn')
+    unlockbase_secro = fields.Char(string='secro')
+    unlockbase_reference = fields.Char(string='reference')
+    unlockbase_servicetag = fields.Char(string='servicetag')
+    unlockbase_icloudemail = fields.Char(string='icloudemail')
+    unlockbase_icloudphone = fields.Char(string='icloudphone')
+    unlockbase_icloududid = fields.Char(string='icloududid')
+    unlockbase_type = fields.Char(string='type')
+    unlockbase_locks = fields.Char(string='locks')
 
     def unlockbase_place_order(self):
         return self.unlockbase_send_action({'Action': 'PlaceOrder'})
